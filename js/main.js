85c4e5dae75e1edcda5379fc3b3e6102da467794
Volodymyr <volodimrmihnyk@gmail.com>
2021-03-08 14:16:02 +0200

    for and whild

--- js/main.js
+++ js/main.js
@@ -9,14 +9,32 @@ const personalMovieDB = {
   private: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// let a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', '');
+//       // c = prompt('Один из последних просмотренных фильмов?', ''),
+//       // d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+      for(let i = 0; i < 2; i++) {
+       const a = prompt('Один из последних просмотренных фильмов?', ''),
+             b = prompt('На сколько оцените его?', '');
+        if(a !=null && b !=null && a !='' && b !='' && a.length < 50) {
+          personalMovieDB.movies[a] = b;
+          console.log('good');
+        } else {
+          console.log('error');
+          i--;
+        }
+      }
+      if (personalMovieDB.count < 10) {
+        console.log('more');
+      } else if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+        console.log('very big');
+      } else if (personalMovieDB.count >= 30) {
+        console.log('well done');
+      } else {
+        console.log('error');
+      }
+
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
-
-    
